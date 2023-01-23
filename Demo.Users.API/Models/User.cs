ca74d24f037ca7b044d707c8054d4c916cb00307
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-23 11:02:36 +0500

    domain model created

---
+++ Demo.Users.API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo.Users.API.Models
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string SureName { get; set; }
+
+        public string Company { get; set; }
+
+        public string Contact { get; set; }
+    }
+}
