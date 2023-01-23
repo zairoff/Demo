f8f2a3b57ee264ff126bff7bd6d633ae665a62f7
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-23 16:22:11 +0500

    consumer services created

---
+++ Demo.Users.API/Services/ContactDeletedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Demo.RabbitMQ.Settings.Models.Contact;
+using Demo.Users.API.Repository;
+using MassTransit;
+
+namespace Demo.Users.API.Services
+{
+    public class ContactDeletedConsumer : IConsumer<ContactDeleted>
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public ContactDeletedConsumer(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public async Task Consume(ConsumeContext<ContactDeleted> context)
+        {
+            var message = context.Message;
+
+            if (message == null) return;
+
+            var user = await _usersRepository.GetAsync(message.UserId);
+
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            user.Contact = string.Empty;
+
+            _usersRepository.Update(user);
+        }
+    }
+}
---
+++ Demo.Users.API/Services/ContactUpdatedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Demo.RabbitMQ.Settings.Models.Contact;
+using Demo.Users.API.Repository;
+using MassTransit;
+
+namespace Demo.Users.API.Services
+{
+    public class ContactUpdatedConsumer : IConsumer<ContactUpdated>
+    {
+        private IUsersRepository _usersRepository;
+
+        public ContactUpdatedConsumer(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public async Task Consume(ConsumeContext<ContactUpdated> context)
+        {
+            var message = context.Message;
+
+            if (message == null) return;
+
+            var user = await _usersRepository.GetAsync(message.UserId);
+
+            if (user == null) { throw new ArgumentNullException(nameof(user)); }
+
+            user.Contact = message.Contact;
+
+            _usersRepository.Update(user);
+        }
+    }
+}
