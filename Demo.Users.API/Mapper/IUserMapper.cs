2ca2cc8e3b0f7750999776f4c92f53d5a1178f11
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-23 11:17:23 +0500

    created & implemented mapper

---
+++ Demo.Users.API/Mapper/IUserMapper.cs
@@ -0,0 +1,12 @@
+﻿using Demo.Users.API.Dtos;
+using Demo.Users.API.Models;
+
+namespace Demo.Users.API.Mapper
+{
+    public interface IUserMapper
+    {
+        User MapUserRequest(UserRequest userRequest);
+
+        UserResponse MapUser(User user);
+    }
+}
---
+++ Demo.Users.API/Mapper/UserMapper.cs
@@ -0,0 +1,38 @@
+﻿using Demo.Users.API.Dtos;
+using Demo.Users.API.Models;
+
+namespace Demo.Users.API.Mapper
+{
+    public class UserMapper : IUserMapper
+    {
+        public UserResponse MapUser(User user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            var userResponse = new UserResponse
+            {
+                Id = user.Id,
+                FirstName = user.Name,
+                LastName = user.SureName,
+                CompanyName = user.Company,
+                Contact = user.Contact
+            };
+
+            return userResponse;
+        }
+
+        public User MapUserRequest(UserRequest userRequest)
+        {
+            if (userRequest == null) throw new ArgumentNullException(nameof(userRequest));
+
+            var user = new User
+            {
+                Name = userRequest.FirstName,
+                SureName = userRequest.LastName,
+                Company = userRequest.CompanyName,
+            };
+
+            return user;
+        }
+    }
+}
