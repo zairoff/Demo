d3fed7403988b723df0e2457ce9e1d5e12908712
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-23 11:08:20 +0500

    created & implemented Repository pattern for data access

---
+++ Demo.Users.API/Repository/IUsersRepository.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Users.API.Models;
+using System.Linq.Expressions;
+
+namespace Demo.Users.API.Repository
+{
+    public interface IUsersRepository
+    {
+        Task AddAsync(User entity);
+        void Delete(User entity);
+        void Update(User entity);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> FindAsync(Expression<Func<User, bool>> expression);
+    }
+}
---
+++ Demo.Users.API/Repository/UsersRepository.cs
@@ -0,0 +1,43 @@
+﻿using Demo.Users.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Demo.Users.API.Repository
+{
+    public class UsersRepository : IUsersRepository
+    {
+        protected readonly UsersDbContext _context;
+
+        public UsersRepository(UsersDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User entity)
+        {
+            await _context.Users.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(User entity)
+        {
+            _context.Users.Remove(entity);
+        }
+
+        public async Task<User> FindAsync(Expression<Func<User, bool>> expression)
+        {
+            return await _context.Users.Where(expression).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public void Update(User entity)
+        {
+            _context.Users.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
