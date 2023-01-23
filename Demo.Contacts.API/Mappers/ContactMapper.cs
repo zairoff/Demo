13dacd12886fd6728fb75432e156827787779a4b
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-23 17:07:03 +0500

    crated mappers

---
+++ Demo.Contacts.API/Mappers/ContactMapper.cs
@@ -0,0 +1,41 @@
+﻿using Demo.Contacts.API.Dtos;
+using Demo.Contacts.API.Models;
+
+namespace Demo.Contacts.API.Mappers
+{
+    public class ContactMapper : IContactMapper
+    {
+        public Contact MapContact(ContactUpdate contactUpdate)
+        {
+            if (contactUpdate == null)
+            {
+                throw new ArgumentNullException(nameof(contactUpdate));
+            }
+
+            var contact = new Contact
+            {
+                Type = contactUpdate.ContactType,
+                ContactInfo = contactUpdate.Contact
+            };
+
+            return contact;
+        }
+
+        public ContactResponse MapContact(Contact contact)
+        {
+            if (contact == null )
+            {
+                throw new ArgumentNullException(nameof(contact));
+            }
+
+            var contactResponse = new ContactResponse
+            {
+                Type = contact.Type,
+                Contact = contact.ContactInfo,
+                User = contact.ContactOwner
+            };
+
+            return contactResponse;
+        }
+    }
+}
---
+++ Demo.Contacts.API/Mappers/IContactMapper.cs
@@ -0,0 +1,11 @@
+﻿using Demo.Contacts.API.Dtos;
+using Demo.Contacts.API.Models;
+
+namespace Demo.Contacts.API.Mappers
+{
+    public interface IContactMapper
+    {
+        Contact MapContact(ContactUpdate contactUpdate);
+        ContactResponse MapContact(Contact contact);
+    }
+}
