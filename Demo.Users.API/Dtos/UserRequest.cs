61d5863f4b28d9a5945648f562ad3077072ec7ad
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-23 11:16:50 +0500

    dtos created

---
+++ Demo.Users.API/Dtos/UserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Users.API.Dtos
+{
+    public class UserRequest
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
---
+++ Demo.Users.API/Dtos/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo.Users.API.Dtos
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Contact { get; set; }
+
+    }
+}
