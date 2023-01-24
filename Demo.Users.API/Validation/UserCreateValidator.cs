1660bc2f19a1e3242e21b74d5bd5bce0041ef177
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-24 09:25:38 +0500

    validate incoming requests

---
+++ Demo.Users.API/Validation/UserCreateValidator.cs
@@ -0,0 +1,23 @@
+﻿using Demo.Users.API.Dtos;
+using FluentValidation;
+
+namespace Demo.Users.API.Validation
+{
+    public class UserCreateValidator : AbstractValidator<UserCreate>
+    {
+        public UserCreateValidator()
+        {
+            this.RuleFor(req => req.FirstName)
+                .NotNull()
+                .NotEmpty();
+
+            this.RuleFor(req => req.LastName)
+                .NotNull()
+                .NotEmpty();
+
+            this.RuleFor(req => req.CompanyName)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
