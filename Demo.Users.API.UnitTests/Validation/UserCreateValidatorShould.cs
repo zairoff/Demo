0368085154c0c3300be4cb6672345774aad5ac0c
marufjon_zoirov <marufjon_zoirov@epam.com>
2023-01-24 09:26:49 +0500

    tests for validator

---
+++ Demo.Users.API.UnitTests/Validation/UserCreateValidatorShould.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using Demo.Users.API.Dtos;
+using Demo.Users.API.Validation;
+using FluentAssertions;
+
+namespace Demo.Users.API.UnitTests.Validation
+{
+    public class UserCreateValidatorShould
+    {
+        private readonly Fixture _fixture;
+        private readonly UserCreateValidator _userCreateValidator;
+
+        public UserCreateValidatorShould()
+        {
+            _fixture= new Fixture();
+            _userCreateValidator= new UserCreateValidator();
+        }
+
+        [Fact]
+        public void PassValidation()
+        {
+            var request = _fixture.Create<UserCreate>();
+
+            var result = _userCreateValidator.Validate(request);
+
+            result.Should().NotBeNull();
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void NotValidRequest()
+        {
+            var request = _fixture.Build<UserCreate>()
+                .Without(x => x.FirstName)
+                .Create();
+
+            var result = _userCreateValidator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+        }
+    }
+}
